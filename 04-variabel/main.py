43c052148fcd3ea97861479b52d2064bab6cf572
Divasatria100 <divasatria700@gmail.com>
2026-09-16 21:26:58 +0700

    Add initial implementation of variable examples and usage in main.py

---
+++ 04-variabel/main.py
@@ -0,0 +1,25 @@
+# Variable adalah tempat menyimpan data sementara yang dapat digunakan dan diubah selama program berjalan. Variabel memiliki nama yang unik dan dapat menyimpan berbagai jenis data seperti angka, teks, atau objek lainnya.
+
+a = 10  # Variabel 'a' menyimpan nilai integer 10
+x = 5
+panjang = 1000
+
+# Pemanggilan pertama
+print("Nilai a = ", a)  # Menampilkan nilai variabel 'a'
+print("Nilai x = ", x)  # Menampilkan nilai variabel 'x'
+print("Nilai panjang = ", panjang)  # Menampilkan nilai variabel 'panjang'
+
+# Penamaan variabel harus mengikuti aturan tertentu, seperti tidak boleh diawali dengan angka, tidak boleh menggunakan spasi, dan sebaiknya menggunakan huruf kecil dengan pemisah garis bawah (_) untuk nama yang terdiri dari beberapa kata.
+
+nilai_y = 15 # Variabel 'nilai_y' menyimpan nilai integer 15
+juta10 = 10000000 # Variabel 'juta10' menyimpan nilai integer 10000000
+nilaiZ = 17.5 # Variabel 'nilaiZ' menyimpan nilai float 17.5
+
+# Pemanggilan kedua 
+print("Nilai a - ", a)  # Menampilkan nilai variabel 'a'
+a = 7
+print("Nilai a setelah diubah = ", a)  # Menampilkan nilai variabel 'a' setelah diubah
+
+# Assignment Indirect (Penugasan Tidak Langsung)
+b = a  # Variabel 'b' menyimpan nilai dari variabel 'a'
+print("Nilai b = ", b)  # Menampilkan nilai variabel 'b'
