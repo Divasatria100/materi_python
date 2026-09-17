c2ce5ba5cb00bf5bbcb48f9e3704e7a0425a9bbe
Diva Satria <divasatria700@gmail.com>
2026-09-17 12:18:50 +0700

    Add examples of if-else statements in main.py

---
+++ 21-if-else/main.py
@@ -0,0 +1,30 @@
+# If dan Else Statement
+
+# 1. if nya
+# 2. kondisinya
+# 3. aksinya
+
+nama = input('Siapa nama anda? ')
+
+# program sebelumnya
+# if kondisi: aksi
+# program selanjutnya
+
+# 1. program if inline
+if nama=="ucup" : print('Kamu Jelek abieeezzzz!!')
+print(f"Terima kasih {nama}")
+
+# 2. program if indentation
+if nama=="ucup":
+    print("kamu jelek abiez!")
+    print("kamu juga jelek banget")
+print(f"Terima kasih {nama}")
+
+# 3. Else Statement
+if nama=="octopus":
+    print("hai octo, si keren!")
+else:
+    print("Ah kamu bukan octo, kamu tidak keren!")
+
+print("akhir dari program")
+
